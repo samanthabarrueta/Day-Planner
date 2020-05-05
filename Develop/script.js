03680323d271ab1fd1027826dce88ee925e96f3b
Samantha <samanthabarrueta@gmail.com>
2020-05-05 15:38:20 -0500

    function for timeblocks
    add currentDay var

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
-    var a = moment().format('dddd, MMMM Do');
-    $('#currentDay').html(a);
+    var currentDay = moment().format('dddd, MMMM Do');
+    $('#currentDay').html(currentDay);
+
+    var createPlanner = function () {
+        var workHours = 9;
+        for (var i = 0; i < workHours; i++) {
+            $('#timeblocks').append(`<div class='row time-block'>
+            <div class='col-md-1 hour description'>
+            </div>
+            <div class='col-md-10 present divider'>
+              <textarea cols='100%'></textarea>
+            </div>
+            <div class='col-md-1 saveBtn'>
+                SAVE
+            </div>
+        `)
+        }
+            
+    }
+
+    createPlanner();
 });
+
+    
