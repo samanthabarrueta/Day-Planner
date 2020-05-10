a9e1672adad32071be7325edf7889c97d38a1ab4
Samantha <samanthabarrueta@gmail.com>
2020-05-10 13:30:39 -0500

    clean up code/ add comments
    rename vars

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 $(document).ready(function () {
-    var a = moment().format('dddd, MMMM Do');
-    $('#currentDay').html(a);
-    var x = moment().format('h : mm a');
-    // $('.hour').html(x);
-    hourBlocks();
+
+    //add date to header
+    var currentDay = moment().format('dddd, MMMM Do');
+    $('#currentDay').html(currentDay);
+        
+    //save item to local storage on click of save button
     $(document).on('click', '.saveBtn', function (event) {
-        var previousElements = $(this).prevAll();
-        console.log(previousElements);
-        var userInput = $(previousElements[0]).children().first().val();
-        var timeSlot = $(previousElements[1]).html();
-        localStorage.setItem(timeSlot, userInput);
-        console.log(time)
-        console.log(userInput)
+        var newSchedItem = $(this).prevAll();
+        var schedItem = $(newSchedItem[0]).children().first().val();
+        var schedHour = $(newSchedItem[1]).html();
+        localStorage.setItem(schedHour, schedItem);
     });
+    
+    //create timeblocks 
     function hourBlocks() {
         var workHour = 9;
         var colorClass = '';
+
         while (workHour < 18) {
             var currentHour = moment().hour();
-            var colorClass = '';
+           
             if ((workHour - currentHour) < 0) {
                 colorClass = 'past';
             } else if (workHour === currentHour) {
@@ -26,14 +27,18 @@ $(document).ready(function () {
             } else {
                 colorClass = 'future';
             }
-            var existingPlanInStorage = localStorage.getItem(workHour);
-            var existingPlan = '';
-            if (existingPlanInStorage != null)
-                existingPlan = existingPlanInStorage;
+
+            var localSchedule = localStorage.getItem(workHour);
+            var scheduleItem = '';
+
+            if (localSchedule !== null)
+                scheduleItem = localSchedule;
+
+            //append timeblocks to html
             $('#timeblocks').append(`<div class='row time-block' id='hour-${workHour}'>
             <div class='col-md-1 hour'>${workHour}</div>
             <div class='col-md-10 divider ${colorClass}'>
-                <textarea class='description' cols='100%' id='planner-${workHour}'>${existingPlan}</textarea>
+                <textarea class='description' cols='100%' id='planner-${workHour}'>${scheduleItem}</textarea>
             </div>
             <div class='col-md-1 saveBtn btn btn-primary'>
                 SAVE
@@ -42,4 +47,5 @@ $(document).ready(function () {
             workHour++;
         }
     }
+    hourBlocks();
 });
