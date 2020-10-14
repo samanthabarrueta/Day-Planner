d1e779a5ae17c46dd657d0fb20745669061a8801
Samantha <samanthabarrueta@gmail.com>
2020-10-13 21:15:32 -0500

    ES6 refactor

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
-$(document).ready(function () {
+const hourBlocks = () => {
+    const currentDay = moment().format('dddd, MMMM Do');
+    $('#currentDay').append(currentDay);
 
-    //add date to header
-    var currentDay = moment().format('dddd, MMMM Do');
-    $('#currentDay').html(currentDay);
+    let workHour = 9;
+    let colorClass = '';
+
+    while (workHour < 18) {
+        let currentHour = moment().hour();
         
-    //save item to local storage on click of save button
-    $(document).on('click', '.saveBtn', function (event) {
-        var newSchedItem = $(this).prevAll();
-        var schedItem = $(newSchedItem[0]).children().first().val();
-        var schedHour = $(newSchedItem[1]).html();
-        localStorage.setItem(schedHour, schedItem);
-    });
-    
-    //create timeblocks 
-    function hourBlocks() {
-        var workHour = 9;
-        var colorClass = '';
-
-        while (workHour < 18) {
-
-            var currentHour = moment().hour();
-            
-            if ((workHour - currentHour) < 0) {
-                colorClass = 'past';
-            } else if (workHour === currentHour) {
-                colorClass = 'present';
-            } else {
-                colorClass = 'future';
-            }
-
-            var localSchedule = localStorage.getItem(workHour);
-            var scheduleItem = '';
-
-            if (localSchedule !== null)
-                scheduleItem = localSchedule;
-
-            //append timeblocks to html
-            $('#timeblocks').append(`<div class='row time-block'>
-            <div class='col-md-1 hour'>${workHour}</div>
-            <div class='col-md-10 divider ${colorClass}'>
-                <textarea class='description' cols='100%'>${scheduleItem}</textarea>
-            </div>
-            <div class='col-md-1 saveBtn btn btn-primary'>
-                SAVE
-            </div>
-            `)
-            workHour++;
+        if ((workHour - currentHour) < 0) {
+            colorClass = 'past';
+        } else if (workHour === currentHour) {
+            colorClass = 'present';
+        } else {
+            colorClass = 'future';
         }
+
+        let localSchedule = localStorage.getItem(workHour);
+        let scheduleItem = '';
+
+        if (localSchedule !== null)
+            scheduleItem = localSchedule;
+
+        $('#timeblocks').append(`
+        <div class='row time-block'>
+        <div class='col-md-1 hour'>${workHour}</div>
+        <div class='col-md-10 divider ${colorClass}'>
+        <textarea class='description' cols='100%'>${scheduleItem}</textarea>
+        </div>
+        <div class='col-md-1 saveBtn btn btn-primary'>
+        SAVE
+        </div>
+        `)
+        workHour++;
     }
-    hourBlocks();
+}
+
+hourBlocks();
+
+$(document).on('click', '.saveBtn', function (event) {
+    let newSchedItem = $(this).prevAll();
+    let schedItem = $(newSchedItem[0]).children().first().val();
+    let schedHour = $(newSchedItem[1]).html();
+    localStorage.setItem(schedHour, schedItem);
 });
+
